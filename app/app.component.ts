36735612edcd9ecc72d94d95ecc86ad168a66020
Jeroen De Swaef <j@recallq.com>
2016-09-18 10:56:46 +0200

    include semantic ui

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <!-- Menu Bar -->
+    <div class="ui menu">
+      <div class="ui container">
+        <a href="#" class="header item">
+          <img class="logo" src="resources/images/factory.png">
+        </a>
+        <div class="header item borderless">
+          <h1 class="ui header">
+            Snippet factory
+          </h1>
+        </div>
+      </div>
+    </div>
+
+    <div class="ui main text container">
+    	Hello
+    </div>
+    `
 })
 export class AppComponent { }
