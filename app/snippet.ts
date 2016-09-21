3f5fb2d4f2d3d46898a74f765c10f22d5686532c
Jeroen De Swaef <j@recallq.com>
2016-09-21 15:07:39 +0200

    removed unused files, fixed typo

--- app/snippet.ts
+++ app/snippet.ts
@@ -5,7 +5,7 @@ export class Snippet {
 	search: string;
 	replace: string;
 
-	constructor(name: string, fileSelector: string; search: string; replace: string) {
+	constructor(name: string, fileSelector: string, search: string, replace: string) {
 		this.name = name;
 		this.fileSelector = fileSelector;
 		this.search = search;
