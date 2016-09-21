f662536c3d7402550bc8bede1c5388632543911f
Jeroen De Swaef <j@recallq.com>
2016-09-21 17:05:00 +0200

    connect to sqlite db

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var Sequelize = require('sequelize');
 var app = express();
 
 app.use(bodyParser.json());  
 app.use(express.static('public'));
+
+var sequelize = new Sequelize('sqlite://snippet-db');
+
+var SnippetTable = sequelize.define('snippet', {
+    name: Sequelize.STRING,
+    fileSelector: Sequelize.STRING,
+    search: Sequelize.STRING,
+    replace: Sequelize.STRING
+});
 
 /*app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -15,6 +24,8 @@ app.post('/add-snippet', function(req, res) {
 	res.json(req.body);
 });
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
+sequelize.sync().then(function() {
+	app.listen(3000, function () {
+		console.log('Example app listening on port 3000!');
+	});
 });
