f9e489badfc3b174e41e4230aa932a1e2c232b3d
Jeroen De Swaef <j@recallq.com>
2016-09-24 15:18:26 +0200

    do first time substitution

--- filesystem-modifier.js
+++ filesystem-modifier.js
@@ -1,24 +1,47 @@
 var _ = require('lodash/core');
 var filewalker = require('filewalker');
+var path = require('path');
+
+var fs = require('fs');
 var snippetService = require('./snippet-service');
 
 function FilesystemModifier(path) {
-	this.path = path;
+	this.basePath = path;
 }
 
 FilesystemModifier.prototype.start = function(doneCallback, errorCallback) {
 	var self = this;
 	snippetService.getAll()
 		.then(function(snippets) {
-			var fileSelectorsRegexList = _.map(snippets, function(snippet) {
-				return new RegExp(snippet.fileSelector);
-			});
-			filewalker(self.path)
-				.on('file', function(path) {
-					//if (path.indexOf('.js') > 0) console.info(".>", path);
-					fileSelectorsRegexList.forEach(function(regExp) {
-						if (regExp.test(path)) {
-							console.info(path);
+			var snippetsById = _.reduce(snippets, function(acc, item) {
+				item.fileRe = new RegExp(item.fileSelector);
+				acc[item.id] = item;
+				return acc;
+			}, {});
+			filewalker(self.basePath)
+				.on('file', function(filePath) {
+					Object.keys(snippetsById).forEach(function(id) {
+						var item = snippetsById[id];
+						if (item.fileRe.test(filePath)) {
+							var fullFilePath = path.join(self.basePath, filePath);
+							fs.readFile(fullFilePath, 'utf8', function(err, contents) {
+								if (err) errorCallback(err);
+								else {
+									var searchRe = new RegExp(item.search, 'g');
+									var match;
+									var newContents = "";
+									var pos = 0;
+									while ((match = searchRe.exec(contents)) !== null) {
+									    //console.info(filePath + ", match found at " + match.index, searchRe.lastIndex, contents.substring(match.index, searchRe.lastIndex));
+										newContents += (contents.substring(pos, match.index) + item.replace.replace('||0||', contents.substring(match.index, searchRe.lastIndex)));
+										pos = searchRe.lastIndex;
+									}
+									newContents += contents.substring(pos, contents.length);
+									fs.writeFile(fullFilePath, newContents, function(err) {
+							    		if (err) errorCallback(err);
+							    	});
+								}
+							});
 						}
 						});
 					})
